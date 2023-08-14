3cfbf04cfb7956adf2b76716336ee922a7e29c0c
Sebastian Danker <108996013+SDanker@users.noreply.github.com>
2023-08-14 15:45:19 -0400

    Add files via upload

---
+++ reclamos_web.py
@@ -0,0 +1,42 @@
+#importar librerias
+import streamlit as st
+import pickle
+import pandas as pd
+import reclamos_model as model
+
+
+
+
+
+def main():
+    #titulo
+    st.title('Modelamiento categorización de reclamos')
+    
+    texto=st.text_input('reclamo','')
+    #title = st.text_input('Movie title', 'Life of Brian')
+
+
+    def resultado(texto):
+        # Frase que deseas analizar
+        #texto = "Me encanta esta película, es realmente emocionante y conmovedora."
+        
+
+        # Realiza el análisis de sentimiento
+        resultados = model.sentiment_analyzer(texto)
+
+        for resultado in resultados:
+            label = resultado['label']
+            score = resultado['score']
+            
+        return f'Sentimiento: {label}, Confianza: {score}'
+
+    if st.button('RUN'):
+       
+        st.write(resultado(texto))
+        
+
+   
+
+if __name__ == '__main__':
+    main()
+    
